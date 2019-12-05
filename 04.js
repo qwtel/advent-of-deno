e06bdd6f2cb06ba54e8444c2aa252ba6e08436df
Florian Klampfer <mail@qwtel.com>
2019-12-05 11:51:24 +0700

    Update day 4

--- 04.js
+++ 04.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env deno --allow-env --importmap=import_map.json
 
 import { read } from './util/aoc.ts';
-import { pipe, distinctUntilChanged, length, groupedUntilChanged, some } from './util/lilit.ts';
+import { pipe, length, groupedUntilChanged, some, unique, filter } from './util/lilit.ts';
 
 // const env = Deno.env();
 
@@ -10,8 +10,7 @@ const [, from, to] = (await read(Deno.stdin))
   .match(/(\d{6})-(\d{6})/)
   .map(Number);
 
-function solve(predicate) {
-  let count = 0;
+function* enum() {
   for (let d1 = 0; d1 <= 9; d1++) {
     for (let d2 = d1; d2 <= 9; d2++) {
       for (let d3 = d2; d3 <= 9; d3++) {
@@ -19,19 +18,27 @@ function solve(predicate) {
           for (let d5 = d4; d5 <= 9; d5++) {
             for (let d6 = d5; d6 <= 9; d6++) {
               const digits = [d1, d2, d3, d4, d5, d6];
-              const n = Number(digits.join(''));
-              if (n >= from && n <= to && predicate(digits, n)) count++;
+              yield [Number(digits.join('')), digits];
             }
           }
         }
       }
     }
   }
-  return count;
 }
 
 // 1
-console.log(solve((digits) => pipe(digits, distinctUntilChanged(), length()) <= 5));
+console.log(pipe(
+  enum(),
+  filter(([n]) => n >= from && n <= to),
+  filter(([, digits]) => pipe(digits, unique(), length()) <= 5),
+  length(),
+));
 
 // 2
-console.log(solve((digits) => pipe(digits, groupedUntilChanged(), some((box) => box.length === 2))));
+console.log(pipe(
+  enum(),
+  filter(([n]) => n >= from && n <= to),
+  filter(([, digits]) => pipe(digits, groupedUntilChanged(), some(g => g.length === 2))),
+  length(),
+));
