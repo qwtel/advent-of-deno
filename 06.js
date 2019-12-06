0333e796c451827a4ff4beab6f2d06abc8d803bf
Florian Klampfer <mail@qwtel.com>
2019-12-06 17:10:06 +0700

    Use BFS instead

--- 06.js
+++ 06.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env deno --allow-env --importmap=import_map.json
 
 import { read } from './util/aoc.ts';
-import { pipe, map, sum, forEach, unique, flatten, min, filter, pairwise } from './util/lilit.ts';
-import { findAndRemove } from './util/other.ts'
+import { pipe, flatten, unique, map, sum } from './util/lilit.ts';
+import { bfs } from './util/graph.ts'
 
 const input = await read(Deno.stdin);
 
@@ -33,44 +33,4 @@ console.log(pipe(
 ))
 
 // 2
-const neighbors = (v) => [...deps.get(v) || [], ...dirs.get(v) || []]
-
-function dijkstra(vertices, source, target) {
-  const q = [];
-  const dist = new Map();
-  const prev = new Map();
-
-  for (const v of vertices) {
-    dist.set(v, Number.POSITIVE_INFINITY);
-    prev.set(v, undefined);
-    q.push(v);
-  }
-  dist.set(source, 0);
-
-  while (q.length) {
-    // slooooooooooooooooooooow
-    const shortest = pipe(q, map(v => dist.get(v)), min());
-    const u = findAndRemove(q, v => dist.get(v) === shortest);
-
-    if (u === target) break;
-
-    for (const v of pipe(neighbors(u), filter(v => q.includes(v)))) {
-      const alt = dist.get(u) + 1; // length is always 1
-      if (alt < dist.get(v)) {
-        dist.set(v, alt);
-        prev.set(v, u);
-      }
-    }
-  }
-
-  const path = []
-  let u = target
-  while (u) {
-    path.unshift(u);
-    u = prev.get(u);
-  }
-
-  return [...pipe(path, pairwise())];
-}
-
-console.log(dijkstra(vertices, 'YOU', 'SAN').length - 2)
+console.log(bfs({ vertices, dirs, deps }, 'YOU', 'SAN').length - 2)
---
+++ util/graph.ts
@@ -0,0 +1,76 @@
+import { pipe, map, min, filter, pairwise, flatten, unique } from "./lilit.ts";
+import { findAndRemove } from "./other.ts";
+
+type Graph = {
+  edges: [string, string][];
+  vertices: string[];
+  dirs: Map<string, string[]>;
+  deps: Map<string, string[]>;
+}
+
+const neighbors = ({ deps, dirs, }: Graph, v: string) => [...deps.get(v) || [], ...dirs.get(v) || []]
+
+const unwind = (prev: Map<string, string>, target: string) => {
+  const path: string[] = [];
+  let u = target;
+  while (u) {
+    path.unshift(u);
+    u = prev.get(u);
+  }
+  return [...pipe(path, pairwise())];
+}
+
+export function bfs(g: Graph, source: string, target: string) {
+  const q = [source];
+  const prev = new Map<string, string>([[source, null]])
+
+  let curr: string;
+  while (curr = q.pop()) {
+    for (const n of neighbors(g, curr)) {
+      if (n === target) {
+        prev.set(n, curr);
+        break;
+      } else if (!prev.has(n)) { 
+        prev.set(n, curr);
+        q.push(n); 
+      }
+    }
+  }
+
+  // TODO: Not found?
+
+  return unwind(prev, target)
+}
+
+export function dijkstra(g: Graph, source: string, target: string) {
+  const { vertices } = g;
+
+  const q = [];
+  const dist = new Map<string, number>();
+  const prev = new Map<string, string>();
+
+  for (const v of vertices) {
+    dist.set(v, Number.POSITIVE_INFINITY);
+    prev.set(v, undefined);
+    q.push(v);
+  }
+  dist.set(source, 0);
+
+  while (q.length) {
+    // slooooooooooooooooooooow
+    const shortest = pipe(q, map(v => dist.get(v)), min());
+    const u = findAndRemove(q, v => dist.get(v) === shortest);
+
+    if (u === target) break;
+
+    for (const v of pipe(neighbors(g, u), filter(v => q.includes(v)))) {
+      const alt = dist.get(u) + 1; // length is always 1
+      if (alt < dist.get(v)) {
+        dist.set(v, alt);
+        prev.set(v, u);
+      }
+    }
+  }
+
+  return unwind(prev, target)
+}
