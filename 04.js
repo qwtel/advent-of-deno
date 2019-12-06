29a226fd816d10a314c8fc96202f426b35001906
Florian Klampfer <mail@qwtel.com>
2019-12-06 22:24:01 +0700

    Simplify day 4

--- 04.js
+++ 04.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S deno --allow-env --importmap=import_map.json
 
 import { read } from './util/aoc.ts';
-import { pipe, length, groupedUntilChanged, some, unique, filter } from './util/lilit.ts';
+import { pipe, length, groupedUntilChanged, some, unique, filter, range, map, pairwise, every } from './util/lilit.ts';
 
 // const env = Deno.env();
 
@@ -10,35 +10,23 @@ const [, from, to] = (await read(Deno.stdin))
   .match(/(\d{6})-(\d{6})/)
   .map(Number);
 
-function* enum() {
-  for (let d1 = 0; d1 <= 9; d1++) {
-    for (let d2 = d1; d2 <= 9; d2++) {
-      for (let d3 = d2; d3 <= 9; d3++) {
-        for (let d4 = d3; d4 <= 9; d4++) {
-          for (let d5 = d4; d5 <= 9; d5++) {
-            for (let d6 = d5; d6 <= 9; d6++) {
-              const digits = [d1, d2, d3, d4, d5, d6];
-              yield [Number(digits.join('')), digits];
-            }
-          }
-        }
-      }
-    }
-  }
-}
+const shared = pipe(
+  range(from, to + 1),
+  map(n => String(n).split('').map(Number)),
+  filter(digits => pipe(digits, pairwise(), every(([a, b]) => b >= a))),
+  Array.from,
+);
 
 // 1
 console.log(pipe(
-  enum(),
-  filter(([n]) => n >= from && n <= to),
-  filter(([, digits]) => pipe(digits, unique(), length()) <= 5),
+  shared,
+  filter(digits => pipe(digits, unique(), length()) <= 5),
   length(),
 ));
 
 // 2
 console.log(pipe(
-  enum(),
-  filter(([n]) => n >= from && n <= to),
-  filter(([, digits]) => pipe(digits, groupedUntilChanged(), some(g => g.length === 2))),
+  shared,
+  filter(digits => pipe(digits, groupedUntilChanged(), some(g => g.length === 2))),
   length(),
 ));
