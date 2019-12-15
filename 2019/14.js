47011af29a945245440b4a38f3569fe08b895237
Florian Klampfer <mail@qwtel.com>
2019-12-15 15:33:57 +0700

    Day 14.2

--- 2019/14.js
+++ 2019/14.js
@@ -3,7 +3,7 @@
 import { read, print } from '../util/aoc.ts';
 import { Array2D } from '../util/array2d.ts';
 import { ValMap } from '../util/values.ts';
-import { pipe, filter, map, constantly, grouped, count, find, every, sum, min } from '../util/lilit.ts';
+import { pipe, filter, map, constantly, grouped, count, find, every, sum, min, range, findIndex } from '../util/lilit.ts';
 import { run } from './intcode.js';
 import { makeGraph, incoming, outgoing, weight } from '../util/graph.ts';
 (async () => {
@@ -19,7 +19,7 @@ const reactions = (await read())
   );
 
 const isIn = (set) => (v) => set.has(v)
-const nearestMultiple = (n, minIncrement) => Math.ceil(n / minIncrement) * minIncrement
+const nextMultiple = (n, minIncrement) => Math.ceil(n / minIncrement) * minIncrement
 
 const minIncrements = new ValMap([['ORE', 1]]);
 const graphData = [];
@@ -29,26 +29,46 @@ for (const [lhs, [[n1, chem1]]] of reactions) {
     graphData.push([chem1, chem2, n2 / n1])
   }
 }
-
 const g = makeGraph(graphData);
 
-const done = new Map([['FUEL', 1]]);
-const queue = ['FUEL'];
-for (const curr of queue) {
-  for (const chem of outgoing(g, curr)) {
-    queue.push(chem)
+function solve(nFuel) {
+  const done = new Map([['FUEL', nFuel]]);
+  const queue = ['FUEL'];
+  for (const curr of queue) {
+    for (const chem of outgoing(g, curr)) {
+      queue.push(chem)
 
-    const allDepsDone = pipe(incoming(g, chem), every(isIn(done)))
-    if (!done.has(chem) && allDepsDone) {
-      const n = pipe(
-        incoming(g, chem),
-        map(c => done.get(c) * weight(g, [c, chem])),
-        sum(),
-      );
-      done.set(chem, nearestMultiple(n, minIncrements.get(chem)))
+      const allDepsDone = pipe(incoming(g, chem), every(isIn(done)))
+      if (!done.has(chem) && allDepsDone) {
+        const n = pipe(
+          incoming(g, chem),
+          map(_ => done.get(_) * weight(g, [_, chem])),
+          sum(),
+          _ => nextMultiple(_, minIncrements.get(chem)),
+        );
+        done.set(chem, n)
+      }
     }
   }
+  return done.get('ORE')
 }
-console.log(done.get('ORE'))
+
+// 1
+console.log(solve(1))
+
+// 2
+function search(f, target, [min, max]) {
+  const middle = Math.floor((min + max) / 2);
+  if (min === middle) return middle;
+  const y = f(middle);
+  if (y > target) return search(f, target, [min, middle]);
+  if (y < target) return search(f, target, [middle, max]);
+  return middle;
+}
+
+const TARGET = 1000000000000;
+let upper = 0;
+for(let x = 1; upper <= TARGET; x *= 2) upper = solve(x)
+console.log(search(solve, TARGET, [1, upper]))
 
 })();
