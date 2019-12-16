47b7ea49897f574e1be2731fdeddd158743b2e4a
Florian Klampfer <mail@qwtel.com>
2019-12-16 19:10:38 +0700

    Day 16.1 update

--- 2019/16.js
+++ 2019/16.js
@@ -4,7 +4,7 @@ import { read, print } from '../util/aoc.ts';
 import { Array2D } from '../util/array2d.ts';
 import { add, addTo, eq, sub, mkNe as notEq } from '../util/vec2d.ts';
 import { ValMap, ValSet } from '../util/values.ts';
-import { pipe, filter, map, concat2, first, zip2, range, sum, cycle, zip, skip, flatMap, constantly, toArray, take, repeat } from '../util/lilit.ts';
+import { pipe, filter, map, concat2, first, zip2, range, sum, cycle, zip, skip, flatMap, constantly, toArray, take, repeat, tap } from '../util/lilit.ts';
 import { notIn, last, notEmpty, wrap } from '../util/other.ts'
 import { run } from './intcode.js';
 (async () => {
@@ -16,31 +16,31 @@ const input = (await read())
   .split('')
   .map(Number);
 
-// console.log(input)
-
 const pattern = [0, 1, 0, -1]
-
-const patternCache = pipe(
-  range(0, input.length),
-  map(i => pipe(pattern, flatMap(n => constantly(n, i + 1)), toArray())),
-  toArray()
-);
 
 let working = [...input]
 for (let phase = 0; phase < 100; phase++) {
-  console.log(phase)
+  if (env.DEBUG && phase % 10 === 0) console.log(phase)
 
   working = pipe(
     range(0, input.length),
-    map(i => {
-      return pipe(
-        zip(working, pipe(cycle(patternCache[i]), skip(1))),
-        map(([a, b]) => a * b),
-        sum(), String, last, Number,
-      )
-    }),
+    map(i => pipe(
+      zip(
+        working,
+        pipe(cycle(pattern), flatMap(n => constantly(n, i + 1)), skip(1)),
+      ),
+      map(([a, b]) => a * b),
+      sum(),
+      Math.abs,
+      _ => _ % 10,
+    )),
     toArray(),
   );
+
+  if (env.DEBUG) {
+    console.log([...working].join(''))
+    console.log('...')
+  }
 }
 
 console.log(pipe(working, take(8), toArray()).join(''))
