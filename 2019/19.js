12e48ca975d52460d8417606000771323b7f3e12
Florian Klampfer <mail@qwtel.com>
2019-12-20 23:02:13 +0700

    Update day 19

--- 2019/19.js
+++ 2019/19.js
@@ -3,7 +3,7 @@
 import { read, print } from '../util/aoc.ts'
 import { Array2D } from '../util/array2d.ts'
 import { ValMap, ValSet } from '../util/values.ts'
-import { pipe, filter, map, constantly, grouped, count, find, every, sum, min, range, findIndex, flatMap, flatten, pluck, toArray, minByKey, product, mapValues, toMap, take, forEach, tap, filterValues, filterSecond, product2, pairwise, zip, skip, mapKeys, last, takeWhile, reverse, findLast, findLastIndex } from '../util/lilit.ts'
+import { pipe, filter, map, constantly, grouped, count, find, every, sum, min, range, findIndex, flatMap, flatten, pluck, toArray, minByKey, product, mapValues, toMap, take, forEach, tap, filterValues, filterSecond, product2, pairwise, zip, skip, mapKeys, last, takeWhile, reverse, findLast, findLastIndex, unzip2, zipMap } from '../util/lilit.ts'
 import { Graph } from '../util/graph2.ts'
 import { addTo, mkNe } from '../util/vec2d.ts'
 import { run } from './intcode.js'
@@ -22,12 +22,9 @@ function solve([sx, sy] = [0, 0], zoom = 1) {
   const coords = pipe(
     product2(range(0, SIZE), range(0, SIZE)),
     map(([x, y]) => [x * zoom + sx, y * zoom + sy]),
-    Array.from,
   );
 
-  const lower = [0, 0];
-  const upper = [SIZE, SIZE]
-  const world = new Array2D([lower, upper])
+  const world = new Array2D([[0, 0], [SIZE, SIZE]])
 
   for (const [x, y] of coords) {
     const { value } = run(input, [x, y]).next();
@@ -43,25 +40,44 @@ function solve([sx, sy] = [0, 0], zoom = 1) {
 pipe(solve(), filter(_ => _ === '#'), count(), console.log)
 
 // 2
-const precision = 10000;
-const world = new Array2D([[0, 0], [precision, 1]], '.');
-for (const x of range(Math.floor(4/5 * precision), precision)) {
-  world.set([x, 0], run(input, [x, precision - 1]).next().value === 1 ? '#' : '.');
+function solve2(precision) {
+  const world = new Array2D([[0, 0], [precision, 1]], '.');
+  for (const x of range(Math.floor(4/5 * precision), precision)) {
+    world.set([x, 0], run(input, [x, precision - 1]).next().value === 1 ? '#' : '.');
+  }
+
+  // console.log('' + world)
+
+  const lastRow = world._array[0];
+  const x1 = pipe(lastRow, findIndex(_ => _ === '#'))
+  const x2 = pipe(lastRow, findLastIndex(_ => _ === '#'))
+  if (env.DEBUG) console.log('x', x1, x2)
+
+  // integer math is hard..
+  const k1 = (precision - 1) / (x1 - 0.5);
+  const k2 = (precision - 1) / (x2 + 0.5);
+  if (env.DEBUG) console.log('k', k1, k2)
+
+  const x = (99.5 * (k2 + 1)) / (k1 - k2);
+  const y = k2 * (x + 99.5);
+  const xx = Math.round(x);
+  const yy = Math.round(y);
+
+  if (env.DEBUG) console.log(x, y)
+  if (env.DEBUG) console.log(xx, yy)
+  return [xx, yy];
 }
-const lastRow = world._array[0];
-const k1 = (precision - 1) / pipe(lastRow, findIndex(_ => _ === '#'));
-const k2 = (precision - 1) / pipe(lastRow, findLastIndex(_ => _ === '#'));
-if (env.DEBUG) console.log('k', k1, k2)
 
-// integer math is hard..
-const x = (99.5 * (k2 + 1)) / (k1 - k2);
-const y = k2 * (x + 99.5);
-const xx = Math.round(x);
-const yy = Math.round(y);
+pipe(solve2(25000), ([x, y]) => 10000 * x + y, console.log)
 
-if (env.DEBUG) console.log(x, y)
-if (env.DEBUG) console.log(xx, yy)
-console.log(xx * 10000 + yy);
+// const drift = 4
+// pipe(
+//   range(1000 + drift, 50000 + drift, 563),
+//   zipMap(solve2), 
+//   map(([i, [xx, yy]]) => [i, [xx - 1122, yy - 1248]]),
+//   forEach(console.log),
+// )
+
 
 // function* descend([xx, yy]) {
 //   let ix = xx;
