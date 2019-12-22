27853ee502ac85a60197ba267b887ecec31f9e0f
Florian Klampfer <mail@qwtel.com>
2019-12-22 16:30:33 +0700

    Speed up 19

--- 2019/19.js
+++ 2019/19.js
@@ -3,9 +3,9 @@
 import { read, print } from '../util/aoc.ts'
 import { Array2D } from '../util/array2d.ts'
 import { ValMap, ValSet } from '../util/values.ts'
-import { pipe, filter, map, constantly, grouped, count, find, every, sum, min, range, findIndex, flatMap, flatten, pluck, toArray, minByKey, product, mapValues, toMap, take, forEach, tap, filterValues, filterSecond, product2, pairwise, zip, skip, mapKeys, last, takeWhile, reverse, findLast, findLastIndex, unzip2, zipMap } from '../util/lilit.ts'
+import { pipe, filter, map, constantly, grouped, count, find, every, sum, min, range, findIndex, flatMap, flatten, pluck, toArray, minByKey, product, mapValues, toMap, take, forEach, tap, filterValues, filterSecond, product2, pairwise, zip, skip, mapKeys, last, takeWhile, reverse, findLast, findLastIndex, unzip2, zipMap, inspect } from '../util/lilit.ts'
 import { Graph } from '../util/graph2.ts'
-import { addTo, mkNe } from '../util/vec2d.ts'
+import { addTo, mkNe, ne } from '../util/vec2d.ts'
 import { run } from './intcode.js'
 (async () => {
 
@@ -40,7 +40,7 @@ function solve([sx, sy] = [0, 0], zoom = 1) {
 pipe(solve(), filter(_ => _ === '#'), count(), console.log)
 
 // 2
-function solve2(precision) {
+function approx2(precision) {
   const world = new Array2D([[0, 0], [precision, 1]], '.');
   for (const x of range(Math.floor(4/5 * precision), precision)) {
     world.set([x, 0], run(input, [x, precision - 1]).next().value === 1 ? '#' : '.');
@@ -68,37 +68,52 @@ function solve2(precision) {
   return [xx, yy];
 }
 
-pipe(solve2(25000), ([x, y]) => 10000 * x + y, console.log)
+// Correct, but slow:
+// pipe(approx2(25000), ([x, y]) => 10000 * x + y, console.log) 
 
-// const drift = 4
+pipe(
+  approx2(250),
+  search,
+  takeWhile(([,, tx, ty]) => tx || ty),
+  findLast(([,, tx, ty]) => tx && ty),
+  ([x, y]) => 10000 * x + y,
+  console.log,
+);
+
+function* search([x, y]) {
+  for (;;) {
+    const tx = run(input, [x + 99, y]).next().value === 1;
+    const ty = run(input, [x, y + 99]).next().value === 1;
+    yield [x, y, tx, ty];
+    if (!tx) x--;
+    if (!ty) y--;
+    if (tx && ty) { x--; y-- };
+  }
+}
+
+// const drift = 5
 // pipe(
-//   range(1000 + drift, 50000 + drift, 563),
+//   range(1000 + drift, 25000 + drift, 563),
 //   zipMap(solve2), 
-//   map(([i, [xx, yy]]) => [i, [xx - 1122, yy - 1248]]),
-//   forEach(console.log),
+//   mapValues(([xx, yy]) => [xx - 1122, yy - 1248]),
+//   tap(console.log),
+//   mapValues(([ex, ey]) => [ex ** 2, ey ** 2]),
+//   Array.from,
+//   _ => {
+//     const n = _.length;
+//     const [xs, ys] = pipe(_, pluck(1), unzip2());
+//     console.log(1 / n * pipe(xs, sum()), 1 / n * pipe(ys, sum()))
+//   }
 // )
 
-
-// function* descend([xx, yy]) {
-//   let ix = xx;
-//   let iy = yy;
-//   for (;;) {
-//     const tx = run(input, [ix + 99, iy]).next().value === 1;
-//     const ty = run(input, [ix, iy + 99]).next().value === 1;
-//     yield [[ix, iy], tx, ty]
-//     if (!tx) ix--;
-//     if (!ty) iy--;
-//     if (tx && ty) { ix--; iy-- };
-//     if (!tx && !ty) break;
-//   }
-// }
-
 // pipe(
-//   descend([xx + 1, yy + 1]),
-//   takeWhile(([, tx, ty]) => tx || ty),
-//   findLast(([, tx, ty]) => tx && ty),
-//   ([[x, y]]) => 10000 * x + y,
-//   console.log
+//   range(0, 249982),
+//   map(i => 249982 - i),
+//   zipMap(solve2), 
+//   mapValues(([xx, yy]) => [xx - 1122, yy - 1248]),
+//   inspect(console.log),
+//   find(([, p]) => ne([0, 0], p)),
+//   console.log,
 // )
 
 })();
