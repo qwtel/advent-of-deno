2f37acbf3b3ade98c417f4c7e853598e924e65de
Florian Klampfer <mail@qwtel.com>
2019-12-23 17:39:16 +0700

    Day 23

---
+++ 2019/23.js
@@ -0,0 +1,113 @@
+#!/usr/bin/env -S deno --allow-env --importmap=../import_map.json
+
+import { read, print } from '../util/aoc.ts'
+import { Array2D } from '../util/array2d.ts'
+import { ValMap, ValSet } from '../util/values.ts'
+import { pipe, filter, map, constantly, grouped, count, find, every, sum, min, range, findIndex, flatMap, flatten, pluck, toArray, minByKey, product, mapValues, toMap, take, forEach, tap, filterValues, filterSecond, product2, pairwise, zip, skip, mapKeys, last, takeWhile, reverse, findLast, findLastIndex, unzip2, zipMap, inspect, cycle, first } from '../util/lilit.ts'
+import { Graph } from '../util/graph2.ts'
+import { addTo, mkNe, ne } from '../util/vec2d.ts'
+import { run } from './intcode2.js'
+(async () => {
+
+const env = Deno.env()
+
+const input = (await read())
+  .trim()
+  .split(',')
+  .map(Number)
+
+const NAT = 50;
+
+class IdleQueue {
+  constructor(iter) {
+    this.q = [...iter];
+    this.idle = this.q.length === 0;
+  }
+  push(...args) {
+    this.q.push(...args);
+    this.idle = false;
+  }
+  *[Symbol.iterator]() {
+    for (;;) {
+      const x = this.q.shift();
+      if (x != null) yield x;
+      else {
+        this.idle = true;
+        yield -1;
+      }
+    }
+  }
+}
+
+class NATQueue {
+  push(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  get idle() { return true; }
+}
+
+class SendBuffer {
+  constructor(qs) {
+    this.qs = qs;
+    this.buffer = [];
+  }
+  push(v) {
+    this.buffer.push(v);
+    if (this.buffer.length === 3) {
+      const [ip, x, y] = this.buffer;
+      this.qs[Math.min(ip, NAT)].push(x, y);
+      this.buffer = [];
+    }
+  }
+}
+
+function* solve() {
+  const queues = new Array(50).fill(null).map((_, i) => new IdleQueue([i]));
+  const buffers = new Array(50).fill(null).map(() => new SendBuffer(queues));
+  const computers = [...pipe(queues, map(q => run(input, q)))];
+
+  queues[NAT] = new NATQueue();
+  buffers[NAT] = new SendBuffer(queues);
+  computers[NAT] = (function* () {
+    for(;;) {
+      if (queues.every(q => q.idle)) {
+        yield 0;
+        yield queues[NAT].x;
+        yield queues[NAT].y;
+      } else yield;
+    }
+  })();
+
+  for(const i of cycle(range(0, 51))) {
+    const { value } = computers[i].next();
+    if (value != null) {
+      if (i === NAT) yield value;
+      buffers[i].push(value);
+    }
+  }
+}
+
+// 1
+pipe(
+  solve(),
+  grouped(3),
+  map(([,, y]) => y),
+  skip(1),
+  first(),
+  console.log,
+);
+
+// 2
+pipe(
+  solve(),
+  grouped(3),
+  map(([,, y]) => y),
+  skip(1),
+  pairwise(),
+  find(([a, b]) => a === b),
+  ([a]) => a,
+  console.log,
+);
+
+})();
---
+++ 2019/intcode2.js
@@ -0,0 +1,121 @@
+import { pad, mod } from '../util/other.ts';
+import { pipe, map, grouped, forEach } from '../util/lilit.ts'
+
+const env = Deno.env()
+
+const print = (mem) => pipe(
+  mem, 
+  map(pad(8, ' ')),
+  grouped(4, 4, true), 
+  forEach(x => console.log(x.join(','))),
+);
+
+const JUMP_IF_TRUE = 5;
+const JUMP_IF_FALSE = 6;
+const LESS_THAN = 7;
+const EQUALS = 8;
+
+export function* run(initialMemory, initialInputs = []) {
+  const mem = [...initialMemory].map(BigInt);
+  let inputs = initialInputs[Symbol.iterator]();
+
+  let pc = 0n;
+  let rb = 0n;
+
+  computer: while (true) {
+    if (env.DEBUG_INT) print(mem);
+
+    const inst = Number(mem[pc++]);
+    const opcode = mod(inst, 100);
+
+    if (opcode === 99) break;
+
+    if (env.DEBUG_INT) console.log('---');
+
+    const getParam = (nr) => {
+      const u = 100 * 10 ** nr;
+      const l = 10 * 10 ** nr;
+      const mode = Math.floor(mod(inst, u) / l);
+      const param = mem[pc++];
+      switch (mode) {
+        case 0: return mem[param] || 0n;
+        case 1: return param;
+        case 2: return mem[rb + param] || 0n;
+        default: throw Error(String(mode));
+      }
+    }
+
+    const setParam = (nr, value) => {
+      const u = 100 * 10 ** nr;
+      const l = 10 * 10 ** nr;
+      const mode = Math.floor(mod(inst, u) / l);
+      const param = mem[pc++];
+      switch (mode) {
+        case 0: mem[param] = value; break;
+        case 2: mem[rb + param] = value; break;
+        case 1: throw Error('Immediate mode not allowed here');
+        default: throw Error(String(mode));
+      }
+    }
+
+    // console.log(opcode);
+    let output = undefined;
+    switch (opcode) {
+      case 1: {
+        const a = getParam(1);
+        const b = getParam(2);
+        setParam(3, a + b);
+        break;
+      }
+      case 2: {
+        const a = getParam(1);
+        const b = getParam(2);
+        setParam(3, a * b);
+        break;
+      }
+      case 3: {
+        const next = inputs.next().value;
+        if (next == null) break computer; 
+        setParam(1, BigInt(next));
+        break;
+      }
+      case 4: {
+        const a = getParam(1);
+        output = Number(a);
+        break;
+      }
+      case JUMP_IF_TRUE: {
+        const a = getParam(1);
+        const b = getParam(2);
+        if (a !== 0n) pc = b
+        break;
+      }
+      case JUMP_IF_FALSE: {
+        const a = getParam(1);
+        const b = getParam(2);
+        if (a === 0n) pc = b
+        break;
+      }
+      case LESS_THAN: {
+        const a = getParam(1);
+        const b = getParam(2);
+        setParam(3, a < b ? 1n : 0n);
+        break;
+      }
+      case EQUALS: {
+        const a = getParam(1);
+        const b = getParam(2);
+        setParam(3, a === b ? 1n : 0n);
+        break;
+      }
+      case 9: {
+        const a = getParam(1);
+        rb += a;
+        break;
+      }
+      default: throw Error(String(opcode));
+    }
+    yield output;
+  }
+  return Number(mem[0]);
+}
