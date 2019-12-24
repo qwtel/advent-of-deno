b4bda80e6def621857e97d79a91819a02b96735d
Florian Klampfer <mail@qwtel.com>
2019-12-24 12:03:47 +0700

    Make graph generic and immutable

--- util/graph2.ts
+++ util/graph2.ts
@@ -1,62 +1,62 @@
 import { pipe, map, min, filter, pairwise, flatten, toArray, sum, flatMap, product2, concat2, toSet } from "./lilit.ts";
 import { findAndRemove } from "./other.ts";
-import { ValMap, ValSet } from "./values.ts";
+import { ValMap, ValSet, is } from "./values.ts";
 
-export type Edge = [string, string];
-export type WeightedEdge = [string, string, number];
-export type GraphData = {
-  vertices: Set<string>;
-  dirs: Map<string, Set<string>>;
-  deps: Map<string, Set<string>>;
-  weights: ValMap<Edge, number>;
-}
+export type Edge<X> = [X, X];
+export type WeightedEdge<X> = [X, X, number];
+export type MaybeWeightedEdge<X> = [X, X, number?];
 
-export class Graph implements GraphData {
-  vertices: Set<string>;
-  dirs: Map<string, Set<string>>;
-  deps: Map<string, Set<string>>;
-  weights: ValMap<[string, string], number>;
+export class Graph<X> {
+  vertices: ValSet<X>;
+  dirs: ValMap<X, ValSet<X>>;
+  deps: ValMap<X, ValSet<X>>;
+  weights: ValMap<[X, X], number>;
 
-  constructor(data: Iterable<WeightedEdge>) {
-    data = [...data];
-    const edges = pipe(data, map(([a, b]) => [a, b] as [string, string]), toArray());
-    this.vertices = new Set([...pipe(edges, flatten<string>())]);
-      
-    this.dirs = new Map(pipe(this.vertices, map(v => [v, new Set()] as [string, Set<string>])));
-    this.deps = new Map(pipe(this.vertices, map(v => [v, new Set()] as [string, Set<string>])));
-    this.weights = new ValMap();
-    for (const [a, b, w] of data) {
-      this.dirs.set(a, this.dirs.get(a).add(b));
-      this.deps.set(b, this.deps.get(b).add(a));
-      this.weights.set([a, b], w ?? 1);
+  constructor(data: Iterable<MaybeWeightedEdge<X>> | Graph<X>) {
+    if (data instanceof Graph) {
+      this.vertices = new ValSet(data.vertices);
+      this.dirs = new ValMap(data.dirs);
+      this.deps = new ValMap(data.deps);
+      this.weights = new ValMap(data.weights);
+    } else {
+      data = [...data];
+      const edges = pipe(data, map(([a, b]) => [a, b] as Edge<X>), toArray());
+      this.vertices = new ValSet([...pipe(edges, flatten<X>())]);
+      this.dirs = new ValMap(pipe(this.vertices, map(v => [v, new ValSet()] as [X, ValSet<X>])));
+      this.deps = new ValMap(pipe(this.vertices, map(v => [v, new ValSet()] as [X, ValSet<X>])));
+      this.weights = new ValMap();
+
+      for (const [a, b, w] of data) {
+        this.dirs.set(a, this.dirs.get(a).add(b));
+        this.deps.set(b, this.deps.get(b).add(a));
+        this.weights.set([a, b], w ?? 1);
+      }
     }
   }
 
-  get edges(): ValSet<Edge> {
-    return pipe(
+  get edges(): ValSet<Edge<X>> {
+    return new ValSet(pipe(
       this.dirs,
-      flatMap(([a, bs]) => pipe(bs, map(b => [a, b] as Edge))),
-      _ => new ValSet(_),
-    );
+      flatMap(([a, bs]) => pipe(bs, map(b => [a, b] as Edge<X>))),
+    ));
   }
 
-  get weightedEdges(): ValSet<WeightedEdge> { 
-    return pipe(
+  get weightedEdges(): ValSet<WeightedEdge<X>> { 
+    return new ValSet(pipe(
       this.dirs,
-      flatMap(([a, bs]) => pipe(bs, map(b => [a, b, this.weight([a, b])] as WeightedEdge))),
-      _ => new ValSet(_),
-    );
+      flatMap(([a, bs]) => pipe(bs, map(b => [a, b, this.weight([a, b])] as WeightedEdge<X>))),
+    ));
   }
 
-  incoming(v: string) { return this.deps.get(v).values(); }
-  outgoing(v: string) { return this.dirs.get(v).values(); }
-  neighbors(v: string) { return concat2(this.incoming(v), this.outgoing(v)); }
+  incoming(v: X) { return this.deps.get(v).values(); }
+  outgoing(v: X) { return this.dirs.get(v).values(); }
+  neighbors(v: X) { return concat2(this.incoming(v), this.outgoing(v)); }
 
-  weight(e: Edge) { return this.weights.get(e); }
+  weight(e: Edge<X>) { return this.weights.get(e); }
   totalWeight () { return pipe(this.weights.values(), sum()); }
 
-  incomingEdges(v: string) { return pipe(this.incoming(v), map(u => [u, v, this.weight([u, v])] as WeightedEdge)); }
-  outgoingEdges(v: string) { return pipe(this.outgoing(v), map(u => [v, u, this.weight([v, u])] as WeightedEdge)); }
+  incomingEdges(v: X) { return pipe(this.incoming(v), map(u => [u, v, this.weight([u, v])] as WeightedEdge<X>)); }
+  outgoingEdges(v: X) { return pipe(this.outgoing(v), map(u => [v, u, this.weight([v, u])] as WeightedEdge<X>)); }
 
 
   // SEARCH
@@ -64,7 +64,7 @@ export class Graph implements GraphData {
 
   // Walks the graph in bfs-like fashion, but will revisit nodes if they are encountered again.
   // Will not terminate for cyclic graphs!
-  *walk(start: string) {
+  *walk(start: X) {
     const q = [start];
     for (const v of q) {
       yield v;
@@ -72,13 +72,13 @@ export class Graph implements GraphData {
     }
   }
   
-  bfs(source: string, target: string) {
+  bfs(source: X, target: X) {
     const q = [source];
-    const prev = new Map<string, string>([[source, null]]);
+    const prev = new ValMap<X, X>([[source, null]]);
 
     for (const curr of q) {
       for (const n of this.outgoing(curr)) {
-        if (n === target) {
+        if (is(n, target)) {
           prev.set(n, curr);
           return unwind(prev, target);
         } else if (!prev.has(n)) { 
@@ -92,11 +92,10 @@ export class Graph implements GraphData {
 
   }
 
-
-  dijkstra(source: string, target: string) {
+  dijkstra(source: X, target: X) {
     const q = [];
-    const dist = new Map<string, number>();
-    const prev = new Map<string, string>();
+    const dist = new ValMap<X, number>();
+    const prev = new ValMap<X, X>();
 
     for (const v of this.vertices) {
       dist.set(v, Number.POSITIVE_INFINITY);
@@ -108,9 +107,9 @@ export class Graph implements GraphData {
     while (q.length) {
       // slooooooooooooooooooooow
       const shortest = pipe(q, map(_ => dist.get(_)), min());
-      const u = findAndRemove(q, v => dist.get(v) === shortest);
+      const u = findAndRemove(q, v => is(dist.get(v), shortest));
 
-      if (u === target) return unwind(prev, target);
+      if (is(u, target)) return unwind(prev, target);
 
       for (const v of pipe(this.outgoing(u), filter(v => q.includes(v)))) {
         const alt = dist.get(u) + this.weight([u, v]);
@@ -167,46 +166,57 @@ export class Graph implements GraphData {
   // MUTATION
   // --------
 
-  addEdge(e: WeightedEdge) {
+  addEdge(e: MaybeWeightedEdge<X>) {
     const [a, b, w] = e;
     this.vertices.add(a).add(b);
-    this.dirs.set(a, (this.dirs.get(a) || new Set()).add(b));
-    this.deps.set(b, (this.deps.get(b) || new Set()).add(a));
+    this.dirs.set(a, (this.dirs.get(a) || new ValSet()).add(b));
+    this.deps.set(b, (this.deps.get(b) || new ValSet()).add(a));
     this.weights.set([a, b], w ?? 1);
     return this;
   }
 
-  addEdges(...es: WeightedEdge[]) {
+  addEdges(...es: MaybeWeightedEdge<X>[]) {
     for (const e of es) this.addEdge(e);
     return this;
   }
 
-  deleteEdge([a, b]: Edge) {
+  deleteEdge([a, b]: Edge<X>) {
     this.dirs.get(a).delete(b);
     this.deps.get(b).delete(a);
     return this.weights.delete([a, b])
   }
 
-  deleteVertex(v: string) {
+  deleteVertex(v: X) {
     for (const [a, b] of this.incomingEdges(v)) this.deleteEdge([a, b]);
     for (const [a, b] of this.outgoingEdges(v)) this.deleteEdge([a, b]);
     return this.vertices.delete(v);
   }
 
-  deleteVertexWithCompaction(v: string) {
+  deleteVertexWithCompaction(v: X) {
     const toAdd = pipe(
       product2(this.incomingEdges(v), this.outgoingEdges(v)),
-      map(([[a, , w1], [, b, w2]]) => [a, b, w1 + w2] as WeightedEdge),
-      filter(([a, b]) => a !== b),
+      map(([[a, , w1], [, b, w2]]) => [a, b, w1 + w2] as WeightedEdge<X>),
+      filter(([a, b]) => !is(a, b)),
       toArray(),
     );
     this.deleteVertex(v);
     this.addEdges(...toAdd);
   }
+
+  copy() {
+    return new Graph(this);
+  }
+
+  equals(b: any) {
+    if (b instanceof Graph) {
+      return is(this.vertices, b.vertices) && is(this.dirs, b.dirs) && is(this.deps, b.deps) && is(this.weights, b.weights);
+    }
+    return false;
+  }
 }
 
-function unwind(prev: Map<string, string>, target: string) {
-  const path: string[] = [];
+function unwind<X>(prev: Map<X, X>, target: X) {
+  const path: X[] = [];
   let u = target;
   while (u) {
     path.unshift(u);
