1a66b4b5c165e635baba80f0fcdbaf83d4541e8b
Florian Klampfer <mail@qwtel.com>
2019-12-25 13:25:02 +0700

    Add POJO support to value collections

--- util/val-funcs.ts
+++ util/val-funcs.ts
@@ -1,10 +1,10 @@
 // @ts-ignore
-import { List as IList, fromJS as _fromJS, is as _is } from 'immutable';
+import { List as IList, Map as IMap, fromJS as _fromJS, is as _is } from 'immutable';
 
 import { ValMap } from './val-map.ts'
 import { ValSet } from './val-set.ts'
 
-import { map } from './iter.ts';
+import { map, mapValues } from './iter.ts';
 
 export type IV = { toJS: any, asMutable: any, get: any, has: any, delete: any, isList: any, [Symbol.iterator]: any };
 
@@ -16,35 +16,35 @@ export function is(a: any, b: any) {
 }
 
 export const fromImmutableKey = <K>(v: IV): K => {
-  if ((IList as any).isList(v)) return [...map(fromImmutableKey)(v)] as unknown as K;
   if (v?.has?.(MAP_TYPE)) return ValMap.of(v) as unknown as K;
   if (v?.has?.(SET_TYPE)) return ValSet.of(v) as unknown as K;
-  // TODO: Plain Object / Record
+  if ((IList as any).isList(v)) return [...map(fromImmutableKey)(v)] as unknown as K;
+  if ((IMap as any).isMap(v)) return Object.fromEntries(mapValues(fromImmutableKey)(v));
   return (v?.toJS?.() ?? v) as K;
 }
 
 export const toImmutableKey = <K>(v: K): IV => {
-  if (v instanceof Array) return IList(v.map(toImmutableKey));
   if (v instanceof ValMap) return v.data;
   if (v instanceof ValSet) return v.data;
-  // TODO: Plain Object / Record
+  if (v instanceof Array) return IList(v.map(toImmutableKey));
+  if (v instanceof Object && v.constructor === Object) return IMap(mapValues(toImmutableKey)(Object.entries(v)));
   return _fromJS(v);
 }
 
 // Same as `fromImmutableKey`, but will leave unknown types untouched.
 export const fromImmutableVal = <K>(v: IV): K => {
-  if ((IList as any).isList(v)) return [...map(fromImmutableKey)(v)] as unknown as K;
   if (v?.has?.(MAP_TYPE)) return ValMap.of(v) as unknown as K;
   if (v?.has?.(SET_TYPE)) return ValSet.of(v) as unknown as K;
-  // TODO: Plain Object / Record
+  if ((IList as any).isList(v)) return [...map(fromImmutableVal)(v)] as unknown as K;
+  if ((IMap as any).isMap(v)) return Object.fromEntries(mapValues(fromImmutableVal)(v));
   return v as unknown as K;
 }
 
 // Same as `toImmutableKey`, but will leave unknown types untouched.
 export const toImmutableVal = <K>(v: K) => {
-  if (v instanceof Array) return IList(v.map(toImmutableKey));
   if (v instanceof ValMap) return v.data;
   if (v instanceof ValSet) return v.data;
-  // TODO: Plain Object / Record
+  if (v instanceof Array) return IList(v.map(toImmutableVal));
+  if (v instanceof Object && v.constructor === Object) return IMap(mapValues(toImmutableVal)(Object.entries(v)));
   return v;
 }
